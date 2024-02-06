b3346a41207898f62392ae0e7aa11316ff1f4c59
Alex Schwartz <alexschwartz01@gmail.com>
2024-02-06 17:25:52 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,6 @@ tar -xf openjdk-22-ea+34_linux-x64_bin.tar.gz
 rm openjdk-22-ea+34_linux-x64_bin.tar.gz
 mv jdk-22/bin/java jdk-22/bin/java-og
 echo 'java-og --source 22 --enable-preview "$@"' > jdk-22/bin/java
+chmod +x jdk-22/bin/java
 sudo mv jdk-22/ /usr/share/
 sudo ln -s /usr/share/jdk-22/bin/java /usr/bin/java
