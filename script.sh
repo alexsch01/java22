72db2bb399f88d9ab3ee33e92f14fb194fd180e3
Alex Schwartz <alexschwartz01@gmail.com>
2024-02-09 15:58:17 -0500

    build 35

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-wget https://download.java.net/java/early_access/jdk22/34/GPL/openjdk-22-ea+34_linux-x64_bin.tar.gz
-tar -xf openjdk-22-ea+34_linux-x64_bin.tar.gz
-rm openjdk-22-ea+34_linux-x64_bin.tar.gz
+wget https://download.java.net/java/GA/jdk22/830ec9fcccef480bb3e73fb7ecafe059/35/GPL/openjdk-22_linux-x64_bin.tar.gz
+tar -xf openjdk-22_linux-x64_bin.tar.gz
+rm openjdk-22_linux-x64_bin.tar.gz
 
 mv jdk-22/bin/java jdk-22/bin/java-og
 echo '/usr/share/jdk-22/bin/java-og --source 22 --enable-preview "$@"' > jdk-22/bin/java
