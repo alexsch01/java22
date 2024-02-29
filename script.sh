2908036bad04d6b023f9861232954181a3098bbc
Alex Schwartz <alexschwartz01@gmail.com>
2024-02-29 11:53:32 -0500

    build 36

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-wget https://download.java.net/java/GA/jdk22/830ec9fcccef480bb3e73fb7ecafe059/35/GPL/openjdk-22_linux-x64_bin.tar.gz
+wget https://download.java.net/java/GA/jdk22/830ec9fcccef480bb3e73fb7ecafe059/36/GPL/openjdk-22_linux-x64_bin.tar.gz
 tar -xf openjdk-22_linux-x64_bin.tar.gz
 rm openjdk-22_linux-x64_bin.tar.gz
 
