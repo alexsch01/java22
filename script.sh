afec0dac06461b775acebbb2375940181b03323d
Alex Schwartz <alexschwartz01@gmail.com>
2024-03-02 13:19:19 -0500

    remove preview features

--- script.sh
+++ script.sh
@@ -2,12 +2,5 @@ wget https://download.java.net/java/GA/jdk22/830ec9fcccef480bb3e73fb7ecafe059/36
 tar -xf openjdk-22_linux-x64_bin.tar.gz
 rm openjdk-22_linux-x64_bin.tar.gz
 
-mv jdk-22/bin/java jdk-22/bin/java-og
-echo '/usr/share/jdk-22/bin/java-og --source 22 --enable-preview "$@"' > jdk-22/bin/java
-chmod +x jdk-22/bin/java
-
 sudo mv jdk-22/ /usr/share/
-sudo ln -s /usr/share/jdk-22/bin/java /usr/bin/java
-
-echo 'complete -f java' >> ~/.bashrc
-source ~/.profile
+sudo ln -s /usr/share/jdk-22/bin/* /usr/bin/
